9695e2090efbe7d82d111c81a0f1deb5e8a6de4d
Thomas Headley <thomasheadleyjr@gmail.com>
2024-02-26 21:21:24 -0500

    Added Test for adding checklist to context

--- ThornTests/Model/CheckListTests.swift
+++ ThornTests/Model/CheckListTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+import SwiftData
 @testable import Thorn
 
 final class CheckListTests: XCTestCase {
@@ -22,5 +23,23 @@ final class CheckListTests: XCTestCase {
     
     expect(name: "Test")
     expect(name: "123")
+  }
+  
+  @MainActor func testNewItemForContext() throws {
+    @MainActor func expect(count: Int) {
+      let config = ModelConfiguration(isStoredInMemoryOnly: true)
+      let container = try! ModelContainer(for: Checklist.self, configurations: config)
+      for x in 1...count {
+        Checklist.newItemForContext(name: "\(x)", context: container.mainContext)
+      }
+      
+      let descriptor = FetchDescriptor<Checklist>()
+      let checklistCount = try! container.mainContext.fetchCount(descriptor)
+      
+      XCTAssertEqual(count, checklistCount)
+    }
+    
+    expect(count: 1)
+    expect(count: 5)
   }
 }
